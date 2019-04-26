b96b19cb432a493ce17ce409e4d05cf8b873928f
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2019-04-26 16:25:35 +0100

    correcting init.py

--- instancegen/__init__.py
+++ instancegen/__init__.py
@@ -5,7 +5,6 @@ from .smps_instance_classes import *
 instances = {
       "rrtailassign" : RRTailAssignInstance,
       "sslp" : SSLPInstance,
-      #"smkp" : SMKPInstance,
       "noswot" : NoswotInstance,
       "snip" : SnipInstance,
       }
